0b67e7c9142f17458592694fa40ab3f0190ca0c3
nmaeva <nmaeva@protonmail.com>
2026-02-27 15:56:26 +0100

    config: additional config to handle jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return React.createElement("h1", {}, "Order App");
+  return <div className="app">App</div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
